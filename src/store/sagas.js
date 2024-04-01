3801bb83772112ff2faaa2ad010265962517aca4
anibesam <samanibe@yahoo.com>
2024-04-01 18:11:34 +0100

    added changes

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -5,7 +5,7 @@ import AccountSaga from "./auth/register/saga"
 import AuthSaga from "./auth/login/saga"
 import ForgetSaga from "./auth/forgetpwd/saga"
 import ProfileSaga from "./auth/profile/saga"
-import LayoutSaga from "./layout/saga"
+import LayoutSaga from "./layout/saga";
 
 export default function* rootSaga() {
   yield all([
