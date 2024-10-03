dbe7fa5b1103a1cfe5d181bd452524ab845e1100
revelation <nusktecsoft@gmail.com>
2024-10-03 01:53:15 +0100

    +Network+

--- src/Network.js
+++ src/Network.js
@@ -58,6 +58,7 @@ class Network {
   }
 }
 //export const baseUrl =  "http://localhost:3000";
+//export const baseUrl =  "http://localhost:3000";
 // export const baseUrl =  "https://ryd-learning-api-v2.onrender.com"
 export const baseUrl =  "https://api-pro.rydlearning.com";
 
