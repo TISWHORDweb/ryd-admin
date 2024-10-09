8ebe7661659f57ef84f50b84c5e2aaee4b462897
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-10-09 15:01:04 +0100

    Package level update ***

--- src/pages/managePackage/index.js
+++ src/pages/managePackage/index.js
@@ -319,6 +319,14 @@ const ManagePackage = () => {
                                   }
                               >
                                 <option value="">Select Level</option>
+                                <option value="12">12</option>
+                                <option value="11">11</option>
+                                <option value="9">9</option>
+                                <option value="8">8</option>
+                                <option value="7">7</option>
+                                <option value="6">6</option>
+                                <option value="5">5</option>
+                                <option value="4">4</option>
                                 <option value="4">4</option>
                                 <option value="3">3</option>
                                 <option value="2">2</option>
